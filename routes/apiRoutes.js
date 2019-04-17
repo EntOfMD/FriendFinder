05bf8b2d8b9ba6324a85e27ff6186811c44f5060
Ashell Fernando <ashellz2010@gmail.com>
2019-04-16 21:33:10 -0400

    testing whether req is recieved

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,12 +2,15 @@ const friends = require('../data/friends');
 
 module.exports = app => {
     app.get('/api/friends', (req, res) => {
-        console.log(friends);
         res.json(friends);
     });
 
     app.post('/api/survey', (req, res) => {
-        friends.push(req.body);
-        res.json(`${res.body} is added to the list`);
+        if (!req.body) {
+            console.log(`Can't find req.body`);
+        } else {
+            friends.push(req.body);
+            res.json(`${req.body.name} was added to the list`);
+        }
     });
 };
