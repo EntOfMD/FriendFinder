961a80283f70169430a8568d5b2811e9f05bd9ed
Ashell Fernando <ashellz2010@gmail.com>
2019-04-16 20:53:44 -0400

    error shouldn't persist anymore

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const bodyParser = require('body-parser'); //parsing middleware
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
-
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
     console.log(`App is online on PORT ${PORT}`);
